7ab3bdac4728207ace8d458fd8192d9d8b5be268
aghni <aghniihsn.gmail.com>
2025-02-11 18:52:29 +0700

    test

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-// const devAPIURL = "http://127.0.0.1:5000"
-const devAPIURL = "http://localhost:3000"
+const devAPIURL = "http://127.0.0.1:5000"
+// const devAPIURL = "http://localhost:3000" //ini dev vercel
 const prodAPIURL = "https://healthmate-be.vercel.app"
-const isProd = false
+const isProd = true
 
 export const getBaseUrl = (url) => {
   if (isProd) return `${prodAPIURL}${url}`
